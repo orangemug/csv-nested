87fdff5ed8ee02d37f00ff45ec578f6fa7f747e0
orangemug <info@orangemug.co.uk>
2014-10-24 12:43:53 +0100

    Added cli.

---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+var fs        = require("fs");
+var csv       = require("fast-csv");
+var sink      = require('stream-sink');
+var yargs     = require("yargs");
+var csvNested = require("../");
+
+var argv = yargs
+  .usage("Parse nested csv.\nUsage: $0 [filepath]")
+  .example("$0 ./path/to/file.csv", "Converts csv to object")
+  .describe("num-headers", "number of headers")
+  .describe("pretty", "pretty print JSON")
+  .default("num-headers", 2)
+  .demand(1)
+  .argv
+
+var inPath = process.cwd()+"/"+argv._[0];
+var numHeaders = argv["num-headers"];
+var pretty = argv["pretty"];
+
+var inStream  = fs.createReadStream(inPath);
+var csvStream = csv();
+
+inStream
+  .pipe(csvStream)
+  .pipe(csvNested(numHeaders))
+  .pipe(sink())
+  .on("data", function(data) {
+    if(pretty) {
+      data = JSON.parse(data);
+      console.log(
+        JSON.stringify(data, null, "  ")
+      );
+    } else {
+      console.log(data);
+    }
+  });
+
